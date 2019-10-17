627c923f1970f8187e588c27f9c8d48496f06a1d
Anthony bassey <39740647+soulzidda@users.noreply.github.com>
2019-10-17 16:04:51 +0200

    adds

---
+++ precache-manifest.cc1af7ea3291ed815df8dc2f5e130a00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3298b981952b48d25d22878cfeb982ab",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6cc05f7af90016c462fb",
+    "url": "/monsters-rolodex/static/css/main.10ecfaf8.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/monsters-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "6cc05f7af90016c462fb",
+    "url": "/monsters-rolodex/static/js/main.6aa6efee.chunk.js"
+  },
+  {
+    "revision": "2dc9b40a277f725e4d87",
+    "url": "/monsters-rolodex/static/js/runtime-main.b05ca383.js"
+  }
+]);
